bfe65b6d7a33c34320a0bb4ae60363ea5088c95e
Pierre Clabot <pierre.clabot@gmail.com>
2023-05-03 10:57:00 +0200

    test

--- planDeSalle.js
+++ planDeSalle.js
@@ -93,8 +93,8 @@ class PlanDeSalle{
     document.addEventListener("touchstart",(e)=>{
       if(e.touches.length>1)
       {
-        e.stopPropagation();
-        e.preventDefault();
+        // e.stopPropagation();
+        // e.preventDefault();
         this.debugL(" !touchemovedocument! ");
       }
     })
